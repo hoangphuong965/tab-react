9b1f19db5ca3905202fefdb95200a1317a24439c
Phuong Hoang <hoangphuong965@gmail.com>
2020-10-29 10:49:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+const url = 'https://course-api.netlify.app/api/react-tabs-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const respon = await fetch(url);
+    const newJob = await respon.json();
+    setJobs(newJob);
+    console.log(newJob);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  if (loading) {
+    return <div class='ui bottom attached loading tab segment'></div>;
+  }
+
+  const { company, title, duties, dates } = jobs[value];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='ui container'>
+      {/* btn */}
+      <div className='ui top attached tabular menu'>
+        <div className='active item cursor'>
+          {jobs.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className={`active item ${
+                  index === value ? 'active-btn' : ''
+                } cursor`}
+                onClick={() => setValue(index)}
+              >
+                {item.company}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      {/* info */}
+      <div className='ui bottom attached active tab segment'>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className='job-date'>{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className='ui segment'>
+              <p>{duty}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 }
 
